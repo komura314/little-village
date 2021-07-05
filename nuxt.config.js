a1b39e62c9bb53af0aeb1b5c102f61c6feeef969
komura <kom314.prog@gmail.com>
2021-07-05 12:46:26 +0000

    feat: システムタイトル、アイコン更新

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,8 +4,8 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   srcDir: 'src/',
   head: {
-    titleTemplate: '%s - src',
-    title: 'src',
+    titleTemplate: '%s',
+    title: 'Little Village',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
